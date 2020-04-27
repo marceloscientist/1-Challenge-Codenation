f15717528b1e7e9cb473cc5bd4713ae28439aecc
Marcelo Santana <marcelo.scientist@hotmail.com>
2020-04-27 14:47:23 -0300

    answer is already going to a file .json

--- public/js/caeser.js
+++ public/js/caeser.js
@@ -4,21 +4,16 @@ var shift;
 function encrypt(text, shift) {
     var result = "";
     if (mode == "ceaser"){
- 
-        //loop through each caharacter in the text
-        for (var i = 0; i < text.length; i++) {
-              
+         //loop through each caharacter in the text
+        for (var i = 0; i < text.length; i++) {            
              //get the character code of each letter
             var c = text.charCodeAt(i);
- 
             // handle uppercase letters
             if(c >= 65 && c <=  90) {
                result += String.fromCharCode((c - 65 + shift) % 26 + 65); 
- 
             // handle lowercase letters
             }else if(c >= 97 && c <= 122){
                 result += String.fromCharCode((c - 97 + shift) % 26 + 97);
- 
             // its not a letter, let it through
             }else {
                 result += text.charAt(i);
--- server.js
+++ server.js
@@ -12,13 +12,20 @@ app.set('port', port);
 
 const server = http.createServer(app);
 const router = express.Router();
-let decifrado = '';
+
+const fs = require('fs');
+let file = fs.readFileSync('answer.json');
+let answer = JSON.parse(file);
 
 const route = router.get('/', (req,res,next) => { 
     axios.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=593785d09c700e4b270eb6572181eea4a8538cd2')
   .then(function (response) {
-    decifrado = caeser.decrypt(response.data.cifrado, response.data.numero_casas);
-    res.send(decifrado);
+    answer = caeser.decrypt(response.data.cifrado, response.data.numero_casas);
+    file = JSON.stringify(answer)
+    fs.writeFile('answer.json', file, (err)=>{
+        console.log('success')
+    })
+    res.send('ok');
   })
   .catch(function (error) {
        console.log(error);
@@ -30,3 +37,4 @@ app.use('/', route);
 server.listen(port); 
 console.log('API rodando na porta ' + port);
 
+// https://www.youtube.com/watch?v=6iZiqQZBQJY
